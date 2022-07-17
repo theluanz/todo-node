c8ac11f4e57a247fd96290ee55dd82effbc41ab4
theluanz <luanzuffo.lz@gmail.com>
2022-07-17 19:35:17 -0300

    Todo endpoints

--- src/index.js
+++ src/index.js
@@ -12,15 +12,23 @@ const users = [];
 function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
   const user = users.find((user) => user.username === username);
+
   if (user) {
     request.user = user;
     return next();
   }
+
   return response.status(400).json({ error: "User not found" });
 }
 
 app.post("/users", (request, response) => {
   const { user, username } = request.body;
+  if (!user || !username) {
+    return response
+      .status(400)
+      .json({ error: "User and username are required" });
+  }
+
   if (users.some((user) => user.username === username)) {
     return response.status(400).json({ error: "User already exists" });
   }
@@ -29,23 +37,61 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { todos } = request.user;
+  return response.json(todos).send();
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+  if (!title || !deadline) {
+    return response
+      .status(400)
+      .json({ error: "Title and deadline are required" });
+  }
+  const todo = {
+    id: uuid(),
+    title: title,
+    deadline: new Date(deadline),
+    done: false,
+    create_at: new Date(),
+  };
+  user.todos.push(todo);
+
+  return response.json(todo).send();
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+  if (!title || !deadline) {
+    return response
+      .status(400)
+      .json({ error: "Title and deadline are required" });
+  }
+  const todo = user.todos.find((todo) => todo.id === id);
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo).send();
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  const { id } = request.params;
+  const todo = user.todos.find((todo) => todo.id === id);
+  todo.done = !todo.done;
+
+  return response.json(todo).send();
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const todo = user.todos.find((todo) => todo.id === id);
+  user.todos.splice(user.todos.indexOf(todo), 1);
 });
 
 module.exports = app;
